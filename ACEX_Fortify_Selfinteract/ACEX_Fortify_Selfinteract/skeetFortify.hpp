a10b59e182cd769945174b8828e3b4f0c40c9ded
Skeet <skeetsf91@gmail.com>
2021-03-26 13:23:26 +1000

    Reduced the size of some presets, list was getting to big

--- ACEX_Fortify_Selfinteract/ACEX_Fortify_Selfinteract/skeetFortify.hpp
+++ ACEX_Fortify_Selfinteract/ACEX_Fortify_Selfinteract/skeetFortify.hpp
@@ -17,15 +17,7 @@ class skeet_Sandbag_D {
   class skeet_HBarrier_D {
   objects[] =
   {
-    {"Land_BagFence_short_F", 5}
-    ,{"Land_BagFence_long_F", 10}
-    ,{"Land_BagFence_round_F", 10}
-    ,{"Land_BagFence_corner_F", 5}
-    ,{"Land_BagFence_end_F", 5}
-    ,{"Land_WoodenShelter_01_F", 20}
-    ,{"Land_BagBunker_small_F", 25}
-    ,{"Land_BagBunker_large_F", 50}
-    ,{"Land_Plank_01_4m_F", 5}
+    {"Land_Plank_01_4m_F", 5}
     ,{"Land_Plank_01_8m_F", 10}
     ,{"Land_Razorwire_F", 15}
     ,{"Land_HBarrierTower_F", 100}
@@ -58,15 +50,7 @@ class skeet_Sandbag_D {
   class skeet_HBarrier_W {
   objects[] =
   {
-    {"Land_BagFence_01_short_green_F", 5}
-    ,{"Land_BagFence_01_long_green_F", 10}
-    ,{"Land_BagFence_01_round_green_F", 10}
-    ,{"Land_BagFence_01_corner_green_F", 5}
-    ,{"Land_BagFence_01_end_green_F", 5}
-    ,{"Land_WoodenShelter_01_F", 20}
-    ,{"Land_BagBunker_01_small_green_F", 25}
-    ,{"Land_BagBunker_01_large_green_F", 50}
-    ,{"Land_Plank_01_4m_F", 5}
+    {"Land_Plank_01_4m_F", 5}
     ,{"Land_Plank_01_8m_F", 10}
     ,{"Land_Razorwire_F", 15}
     ,{"Land_HBarrier_01_big_tower_green_F", 100}
@@ -90,26 +74,5 @@ class skeet_Sandbag_D {
     ,{"Land_Plank_01_4m_F", 5}
     ,{"Land_Plank_01_8m_F", 10}
     ,{"Land_Razorwire_F", 15}
-  };
-};
-  class skeet_Trash {
-      objects[] =
-  {
-    {"Land_PortableLight_single_F", 5}
-    ,{"Land_PortableLight_double_F", 5}
-    ,{"Land_LampHalogen_F", 5}
-    ,{"RoadCone_L_F", 5}
-    ,{"RoadCone_F", 5}
-    ,{"Land_Camping_Light_F", 5}
-    ,{"Land_Target_Dueling_01_F", 5}
-    ,{"TargetP_Inf_F", 5}
-    ,{"TargetP_Inf2_F", 5}
-    ,{"plp_bo_RadioSixties", 5}
-    ,{"Windsock_01_F", 5}
-    ,{"PortableHelipadLight_01_green_F", 5}
-    ,{"Land_CampingChair_V1_F", 5}
-    ,{"Land_Misc_deerstand", 25}
-    ,{"Land_Plank_01_4m_F", 5}
-    ,{"Land_Plank_01_8m_F", 10}
   };
 };
